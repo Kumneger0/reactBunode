025f5995ce67da7871f7e2b61121b3739876e8bc
kumneger0 <kumnegerwondimu01@gmail.com>
2024-03-11 23:53:44 +0300

    chore : made some initial layout for docs page

--- packages/docs/app/layout.tsx
+++ packages/docs/app/layout.tsx
@@ -21,7 +21,7 @@ function Layout({ children }: { children: React.ReactNode }) {
 					rel="stylesheet"
 				/>
 			</head>
-			<body className="mx-auto text-white bg-gray-900   m-0 p-0 h-screen overflow-x-hidden">
+			<body className="mx-auto text-white bg-gray-800 m-0 p-0 h-screen overflow-x-hidden">
 				<div className="max-w-6xl mx-auto">{children}</div>
 			</body>
 		</html>
--- packages/docs/app/page.tsx
+++ packages/docs/app/page.tsx
@@ -13,8 +13,144 @@ async function Page() {
 	return (
 		<>
 			<h1 className="w-full text-center capitalize">docs for ReactBunode</h1>
-			<div className="flex relative flex-wrap justify-center gap-5">
-				// TODO: MAKE Documentaion Site
+			<div className="flex  relative flex-wrap justify-center gap-5">
+				<div className="w-1/3 flex items-center flex-col gap-1 top-3 max-h-[600px] sticky">
+					<div className="font-bold  text-lg">
+						<a className="text-white text-decoration-none" href="#get-started">
+							Get Started
+						</a>
+					</div>
+					<div className="font-bold  text-lg">
+						<a className="text-white text-decoration-none" href="#Installation">
+							Installation
+						</a>
+					</div>
+					<div className="font-bold  text-lg">
+						<a className="text-white text-decoration-none" href="#Setting-up-new-Project">
+							Setting up new Project
+						</a>
+					</div>
+					<div className="font-bold  text-lg">
+						<a className="text-white text-decoration-none" href="#Pages">
+							Pages
+						</a>
+					</div>
+					<div className="font-bold  text-lg">
+						<a className="text-white text-decoration-none" href="#Routes">
+							Routes
+						</a>
+					</div>
+					<div className="font-bold  text-lg">
+						<a className="text-white text-decoration-none" href="#functions">
+							functions
+						</a>
+					</div>
+					<div className="font-bold  text-lg">
+						<a className="text-white text-decoration-none" href="#Metadata">
+							Metadata
+						</a>
+					</div>
+				</div>
+				<div className="w-[60%]">
+					<div id="get-started" className="p-2 my-3">
+						<h1 className="font-bold text-xl">Get Started</h1>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at atque voluptates
+							architecto aliquid nisi a placeat ea repellendus numquam hic, sint culpa tempora
+							facere exercitationem tenetur maiores labore voluptatum beatae blanditiis expedita
+							deserunt saepe soluta distinctio? Dolore commodi exercitationem dolores blanditiis.
+							Odit, velit. Ea voluptatem laudantium, repellat velit aliquid magni odio eum sunt
+							maiores fuga autem dolores similique atque suscipit, quibusdam accusantium nesciunt
+							reprehenderit vitae. Soluta iste repellendus error facilis assumenda, accusamus
+							voluptatem cupiditate. Error consectetur tenetur eaque eveniet, rem esse sit dolores
+							nesciunt non eum maxime, magnam
+						</div>
+					</div>
+					<div id="Installation" className="p-2 my-3">
+						<h1 className="font-bold text-xl">Installation</h1>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at atque voluptates
+							architecto aliquid nisi a placeat ea repellendus numquam hic, sint culpa tempora
+							facere exercitationem tenetur maiores labore voluptatum beatae blanditiis expedita
+							deserunt saepe soluta distinctio? Dolore commodi exercitationem dolores blanditiis.
+							Odit, velit. Ea voluptatem laudantium, repellat velit aliquid magni odio eum sunt
+							maiores fuga autem dolores similique atque suscipit, quibusdam accusantium nesciunt
+							reprehenderit vitae. Soluta iste repellendus error facilis assumenda, accusamus
+							voluptatem cupiditate. Error consectetur tenetur eaque eveniet, rem esse sit dolores
+							nesciunt non eum maxime, magnam
+						</div>
+					</div>
+					<div id="Setting-up-new-Project" className="p-2 my-3">
+						<h1 className="font-bold text-xl">Setting up new Project</h1>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at atque voluptates
+							architecto aliquid nisi a placeat ea repellendus numquam hic, sint culpa tempora
+							facere exercitationem tenetur maiores labore voluptatum beatae blanditiis expedita
+							deserunt saepe soluta distinctio? Dolore commodi exercitationem dolores blanditiis.
+							Odit, velit. Ea voluptatem laudantium, repellat velit aliquid magni odio eum sunt
+							maiores fuga autem dolores similique atque suscipit, quibusdam accusantium nesciunt
+							reprehenderit vitae. Soluta iste repellendus error facilis assumenda, accusamus
+							voluptatem cupiditate. Error consectetur tenetur eaque eveniet, rem esse sit dolores
+							nesciunt non eum maxime, magnam
+						</div>
+					</div>
+					<div id="Pages" className="p-2 my-3">
+						<h1 className="font-bold text-xl">Pages</h1>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at atque voluptates
+							architecto aliquid nisi a placeat ea repellendus numquam hic, sint culpa tempora
+							facere exercitationem tenetur maiores labore voluptatum beatae blanditiis expedita
+							deserunt saepe soluta distinctio? Dolore commodi exercitationem dolores blanditiis.
+							Odit, velit. Ea voluptatem laudantium, repellat velit aliquid magni odio eum sunt
+							maiores fuga autem dolores similique atque suscipit, quibusdam accusantium nesciunt
+							reprehenderit vitae. Soluta iste repellendus error facilis assumenda, accusamus
+							voluptatem cupiditate. Error consectetur tenetur eaque eveniet, rem esse sit dolores
+							nesciunt non eum maxime, magnam
+						</div>
+					</div>
+					<div id="Routes" className="p-2 my-3">
+						<h1 className="font-bold text-xl">Routes</h1>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at atque voluptates
+							architecto aliquid nisi a placeat ea repellendus numquam hic, sint culpa tempora
+							facere exercitationem tenetur maiores labore voluptatum beatae blanditiis expedita
+							deserunt saepe soluta distinctio? Dolore commodi exercitationem dolores blanditiis.
+							Odit, velit. Ea voluptatem laudantium, repellat velit aliquid magni odio eum sunt
+							maiores fuga autem dolores similique atque suscipit, quibusdam accusantium nesciunt
+							reprehenderit vitae. Soluta iste repellendus error facilis assumenda, accusamus
+							voluptatem cupiditate. Error consectetur tenetur eaque eveniet, rem esse sit dolores
+							nesciunt non eum maxime, magnam
+						</div>
+					</div>
+					<div id="functions" className="p-2 my-3">
+						<h1 className="font-bold text-xl">functions</h1>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at atque voluptates
+							architecto aliquid nisi a placeat ea repellendus numquam hic, sint culpa tempora
+							facere exercitationem tenetur maiores labore voluptatum beatae blanditiis expedita
+							deserunt saepe soluta distinctio? Dolore commodi exercitationem dolores blanditiis.
+							Odit, velit. Ea voluptatem laudantium, repellat velit aliquid magni odio eum sunt
+							maiores fuga autem dolores similique atque suscipit, quibusdam accusantium nesciunt
+							reprehenderit vitae. Soluta iste repellendus error facilis assumenda, accusamus
+							voluptatem cupiditate. Error consectetur tenetur eaque eveniet, rem esse sit dolores
+							nesciunt non eum maxime, magnam
+						</div>
+					</div>
+					<div id="Metadata" className="p-2 my-3">
+						<h1 className="font-bold text-xl">Metadata</h1>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero at atque voluptates
+							architecto aliquid nisi a placeat ea repellendus numquam hic, sint culpa tempora
+							facere exercitationem tenetur maiores labore voluptatum beatae blanditiis expedita
+							deserunt saepe soluta distinctio? Dolore commodi exercitationem dolores blanditiis.
+							Odit, velit. Ea voluptatem laudantium, repellat velit aliquid magni odio eum sunt
+							maiores fuga autem dolores similique atque suscipit, quibusdam accusantium nesciunt
+							reprehenderit vitae. Soluta iste repellendus error facilis assumenda, accusamus
+							voluptatem cupiditate. Error consectetur tenetur eaque eveniet, rem esse sit dolores
+							nesciunt non eum maxime, magnam
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
